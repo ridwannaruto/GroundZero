afed6d420008645644c046997392f0bcec30caaf
Sabra Ossen <sabraosn@gmail.com>
2013-10-07 16:22:29 +0530

    contacts version 1

--- app/cache/dev/twig/0f/e1/6c486c820a2507e5c29c93e0c4de.php
+++ app/cache/dev/twig/0f/e1/6c486c820a2507e5c29c93e0c4de.php
@@ -42,9 +42,36 @@ class __TwigTemplate_0fe16c486c820a2507e5c29c93e0c4de extends Twig_Template
     public function block_body($context, array $blocks = array())
     {
         // line 7
-        echo "Contacts
-
-";
+        echo "    ";
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["result"]) ? $context["result"] : $this->getContext($context, "result")));
+        foreach ($context['_seq'] as $context["_key"] => $context["res"]) {
+            // line 8
+            echo "        <li>";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["res"]) ? $context["res"] : $this->getContext($context, "res")), "firstname", array(), "array"), "html", null, true);
+            echo "</li>
+        <li>";
+            // line 9
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["res"]) ? $context["res"] : $this->getContext($context, "res")), "lastname", array(), "array"), "html", null, true);
+            echo "</li>
+        <li>";
+            // line 10
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["res"]) ? $context["res"] : $this->getContext($context, "res")), "email", array(), "array"), "html", null, true);
+            echo "</li>
+        <li>";
+            // line 11
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["res"]) ? $context["res"] : $this->getContext($context, "res")), "mobilenumber", array(), "array"), "html", null, true);
+            echo "</li>
+        <li>";
+            // line 12
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["res"]) ? $context["res"] : $this->getContext($context, "res")), "accesslevel", array(), "array"), "html", null, true);
+            echo "</li>
+        <br>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['res'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
     }
 
     public function getTemplateName()
@@ -59,6 +86,6 @@ class __TwigTemplate_0fe16c486c820a2507e5c29c93e0c4de extends Twig_Template
 
     public function getDebugInfo()
     {
-        return array (  45 => 7,  42 => 6,  36 => 4,  30 => 3,);
+        return array (  67 => 12,  63 => 11,  59 => 10,  55 => 9,  50 => 8,  45 => 7,  42 => 6,  36 => 4,  30 => 3,);
     }
 }
--- src/VolunteerManagementSystem/PagesBundle/Controller/ContactpageController.php
+++ src/VolunteerManagementSystem/PagesBundle/Controller/ContactpageController.php
@@ -8,14 +8,18 @@ use Symfony\Component\HttpFoundation\Request;
 class ContactpageController extends Controller{
     
     public function contactAction(Request $request){
-        $id = $request->get('id');
+         $id = $request->get('id');
         $em = $this->getDoctrine()->getEntityManager();
         $repository = $em->getRepository('VolunteerManagementSystemRegistrationBundle:User');
 
         $user = $repository->findOneBy(array('id' => $id));
         if($user){
-           
-           return $this->render('VolunteerManagementSystemPagesBundle:Contactpage:contactpage.html.twig', array('id' => $id));
+           $stmt = $this->getDoctrine()->getEntityManager()
+                        ->getConnection()
+                        ->prepare('select firstname,lastname,accesslevel,email,mobilenumber from user');
+                        $stmt->execute();
+                        $result = $stmt->fetchAll();
+           return $this->render('VolunteerManagementSystemPagesBundle:Contactpage:contactpage.html.twig', array('id' => $id,'result'=> $result));
        }
         else{
             
